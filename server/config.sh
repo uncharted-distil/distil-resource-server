3fd9ecf0cef56f5f5cd6a0efbcfaf29e2c74b373
Chris Bethune <cbethune@uncharted.software>
2018-06-25 16:44:14 -0400

    fixes missing images

--- server/config.sh
+++ server/config.sh
@@ -2,13 +2,13 @@
 
 # name and version of docker image that will be created
 DOCKER_IMAGE_NAME=distil_res_server
-DOCKER_IMAGE_VERSION=0.2.0
+DOCKER_IMAGE_VERSION=0.2.1
 
 # datasets to ingest
 DATASETS=(22_handgeometry)
 
 # path to data on local system (ingest from HDFS not currently supported)
-HOST_DATA_DIR=~/datasets/seed_datasets_current
+HOST_DATA_DIR=~/data/d3m
 
 # path to data in the docker container
 CONTAINER_DATA_DIR=/data
