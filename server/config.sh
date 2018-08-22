7fe6e0624556d09a52c2282ef04d098db376daa9
Chris Bethune <cbethune@uncharted.software>
2018-08-22 20:55:26 +0000

    adds fudan pedestrian images

--- server/config.sh
+++ server/config.sh
@@ -2,13 +2,13 @@
 
 # name and version of docker image that will be created
 DOCKER_IMAGE_NAME=distil_res_server
-DOCKER_IMAGE_VERSION=0.2.3
+DOCKER_IMAGE_VERSION=0.3.0
 
 # datasets to ingest
-DATASETS=(22_handgeometry)
+DATASETS=(LL1_penn_fudan_pedestrian 22_handgeometry)
 
 # path to data on local system (ingest from HDFS not currently supported)
-HOST_DATA_DIR=~/data/d3m
+HOST_DATA_DIR=~/datasets/seed_datasets_current
 
 # path to data in the docker container
 CONTAINER_DATA_DIR=/data
